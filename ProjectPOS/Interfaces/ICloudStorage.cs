969b9e190a5ddc5bf2496266744bcec742f29cb6
Andrzejek12 <zoladowski.lukasz@gmail.com>
2021-06-06 22:22:35 +0200

    add Interfaces

---
+++ ProjectPOS/Interfaces/ICloudStorage.cs
@@ -0,0 +1,18 @@
+﻿using ProjectPOS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProjectPOS.Interfaces
+{
+    public interface ICloudStorage
+    {
+        IEnumerable<ViewModel> GetFiles();
+        bool DeleteFile(string file, string fileExtension);
+        bool UploadFile(HttpPostedFileBase blobFile);
+
+        Task<bool> DownloadFileAsync(string file, string fileExtension);
+    }
+}
