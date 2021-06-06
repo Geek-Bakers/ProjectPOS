cf81aeb7ec001d4944c3765e630b810d5b4ef0d9
Andrzejek12 <zoladowski.lukasz@gmail.com>
2021-06-06 21:59:58 +0200

    add ViewModel

---
+++ ProjectPOS/Models/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ProjectPOS.Models
+{
+    public class ViewModel
+    {
+        public string ContainerName { get; set; }
+        public string StorageUri { get; set; }
+        public string FileName { get; set; }
+        public string PrimaryUri { get; set; }
+        public string fileExtenion { get; set; }
+
+        public string FileNameWithoutExt
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(FileName);
+            }
+        }
+
+        public string FileNameExtensionOnly
+        {
+            get
+            {
+                return System.IO.Path.GetExtension(FileName).Substring(1);
+            }
+        }
+    }
+}
